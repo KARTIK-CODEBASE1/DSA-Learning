ebabcc693ace3aecb3493390a32a5667b727c111
KARTIK-CODEBASE1 <kartik46787684@gmail.com>
2026-07-27 22:25:22 +0530

    Solve maximum subarray using brute force and Kadane's algorithm

--- array/kadanesAlgo.cpp
+++ array/kadanesAlgo.cpp
@@ -1,22 +1,65 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    int n = 7;
-    int arr[n] = {3,-4,5,4,-1,7,-8};
+int main()
+{
 
+    /*BASICS OF SUBARRAY CONCEPT
+
+        int n=5;
+        int arr[5]= {1,2,3,4,5};
+
+        for(int st=0; st<n; st++){
+            for(int end=st;end<n;end++){
+                for(int i=st; i<=end; i++){
+                    cout<< arr[i];
+                }
+                cout << " ";
+            }
+            cout << endl;
+        }
+
+    */
+
+    /* Maximum Subarray Sum
+
+        int n=7;
+        int arr[n]= {3,-4,5,4,-1,7,-8};
+
+        int maxSum = INT_MIN;
+
+        for(int st=0; st<n; st++){
+            int currSum = 0;
+            for(int end=st;end<n;end++){
+                currSum += arr[end];
+                maxSum = max(currSum, maxSum);
+            }
+        }
+
+        cout<< "The maximum subarray sum is :" <<maxSum;
+
+    */
+
+    int n = 7;
+    int arr[] = {3, -4, 5, 4, -1, 7, -8};
+
+    int currSum = 0;
     int maxSum = INT_MIN;
-    for(int st=0; st<n; st++){
-        int currentSum = 0;
-        for(int end=st; end<n; end++){
-            currentSum += arr[end];
-            maxSum = max(currentSum, maxSum);
-         }
+
+    for (int i = 0; i < n; i++)
+    {
+        currSum += arr[i];
+        maxSum = max(maxSum, currSum);
+
+        if (currSum < 0)
+        {
+            currSum = 0;
+        }
     }
 
-    cout<<"max subarray sum = "<< maxSum << endl;
+    cout << "Maximum Sum = " << maxSum;
 
     return 0;
 }
